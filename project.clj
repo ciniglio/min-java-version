4a99d169958561201df49fee2ed77918ca02c5c6
Alejandro Ciniglio <mail@alejandrociniglio.com>
2014-07-18 14:17:41 -0400

    add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject min-java-version "0.1.0-SNAPSHOT"
   :description "Leiningen plugin to abort if JDK is below min version"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/ciniglio/min-java-version"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
